d3139605ab3b4030d60dd839ab31ed01bad31157
ANORA\manojkumar.v <manojkumar.v@anorasolutions.com>
2026-03-18 10:23:39 +0530

    RESOLVE THEIRS

--- area.c
+++ area.c
@@ -1,14 +1,22 @@
 #include<stdio.h>
+<<<<<<< HEAD
 
 float circle( float radius);
 
 
+=======
+float circle( float radius);
+>>>>>>> eccd53b (RESOLVE THEIRS)
 float rectangle( float len, float breadth);
 
 int main()
 {
+<<<<<<< HEAD
 
 float a,b,r;
+=======
+flaot a,b,r;
+>>>>>>> eccd53b (RESOLVE THEIRS)
 printf("Enter the length:");scanf("%f",&a);
 printf("Enter the breadth:");scanf("%f",&b);
 area=rectangle(a,b);
@@ -17,6 +25,7 @@ printf("Area of rectangle:%f",area);
 printf("Enter the radius");scanf("%f",&r);
 area=circle(r);
 printf("Area of circle:%f",area);
+<<<<<<< HEAD
 }
 
 float circle(float radius){
@@ -30,4 +39,10 @@ printf("Area of rectangle:%d",area);
 float rectangle(float a, float b){
     return (a*b);
 
+=======
+}
+
+float circle(float radius){
+    return (3.14*radius*radius);
+>>>>>>> eccd53b (RESOLVE THEIRS)
 }
