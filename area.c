dc07aa85df8dcc64c6111f73bb0844a01109aa05
ANORA\manojkumar.v <manojkumar.v@anorasolutions.com>
2026-03-18 09:42:22 +0530

    function for area of rectangle

--- area.c
+++ area.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 
-float rectangle( float len, float breadth)
+float rectangle( float len, float breadth);
 
 int main()
 {
 printf("Enter the length:");scanf("%d",&a);
 printf("Enter the breadth:");scanf("%d",&b);
-area=rectangle(a,b);
+float area=rectangle(a,b);
 printf("Area of rectangle:%d",area);
+}
+float rectangle(float a, float b){
+    return (a*b);
 }
