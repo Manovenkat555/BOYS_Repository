5533701df11dad08eef9716816cc0159aef5e417
KABILAN U <kabilan1835@gmail.com>
2026-03-18 10:50:22 +0530

    conflict resolve

--- area.c
+++ area.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
+
 float circle( float radius);
+
+
 float rectangle( float len, float breadth);
 
 int main()
 {
-flaot a,b,r;
+
+float a,b,r;
 printf("Enter the length:");scanf("%f",&a);
 printf("Enter the breadth:");scanf("%f",&b);
 area=rectangle(a,b);
@@ -17,4 +21,13 @@ printf("Area of circle:%f",area);
 
 float circle(float radius){
     return (3.14*radius*radius);
+
+printf("Enter the length:");scanf("%d",&a);
+printf("Enter the breadth:");scanf("%d",&b);
+float area=rectangle(a,b);
+printf("Area of rectangle:%d",area);
+}
+float rectangle(float a, float b){
+    return (a*b);
+
 }
