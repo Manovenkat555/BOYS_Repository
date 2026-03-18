a7fadafa9b0bc05437f86c8b30d8619bece8cda2
karthikeyant <karthikeyant142@gmail.com>
2026-03-18 09:24:35 +0530

    area c code

---
+++ area.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+float rectangle( float len, float breadth)
+
+int main()
+{
+printf("Enter the length:");scanf("%d",&a);
+printf("Enter the breadth:");scanf("%d",&b);
+area=rectangle(a,b);
+printf("Area of rectangle:%d",area);
+}
